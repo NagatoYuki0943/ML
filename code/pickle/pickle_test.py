5ed05797b71316c89debe98bcb2ce57b49352925
Yuki <workcomputer.com>
2023-11-29 10:13:36 +0800

    pickle test

---
+++ code/pickle/pickle_test.py
@@ -0,0 +1,18 @@
+import pickle
+import numpy as np
+
+
+x = np.arange(6).reshape(2, 3)
+
+
+# 保存文件, mode为b
+with open("pickle.pkl", mode="wb") as f:
+    pickle.dump(x, f)
+
+
+# pickle读文件, mode为b
+with open("pickle.pkl", mode="rb") as f:
+    y = pickle.load(f)
+
+
+print(np.all(x == y))   # True

084d3d134a6e410f6e87d7e0d2253f03871cfb0a
yuki <lihongtu12138@outlook.com>
2024-04-15 13:51:47 +0800

    gradio更新功能

---
+++ code/gradio/chatbot.py
@@ -0,0 +1,12 @@
+# https://www.gradio.app/playground
+
+import random
+import gradio as gr
+
+def random_response(message, history):
+    return random.choice(["Yes", "No"])
+
+demo = gr.ChatInterface(random_response)
+
+if __name__ == "__main__":
+    demo.launch()
--- code/gradio/test1.py
+++ code/gradio/test1.py
@@ -15,4 +15,6 @@ textbox = gr.Textbox()
 chatbox = gr.Chatbot()
 
 demo = gr.Interface(fn=chat, inputs=[textbox], outputs=[chatbox])
-demo.launch()
+# 设置队列启动，队列最大长度为 100
+demo.queue(max_size=100)
+demo.launch(server_name="127.0.0.1", server_port=7860)

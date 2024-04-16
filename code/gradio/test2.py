869a9beeb94c707af682e47fa567f9c2fe6b6277
yuki <lihongtu12138@outlook.com>
2024-04-16 09:52:41 +0800

    对话一次性输出成功

--- code/gradio/test2.py
+++ code/gradio/test2.py
@@ -8,25 +8,26 @@ import numpy as np
 
 def chat(
     query: str,
-    history: list,  # [['What is the capital of France?', 'The capital of France is Paris.'], ['Thanks', 'You are Welcome']]
+    history: None | list,  # [['What is the capital of France?', 'The capital of France is Paris.'], ['Thanks', 'You are Welcome']]
     max_new_tokens: int = 1024,
     top_p: float = 0.8,
     top_k: int = 40,
     temperature: float = 0.8,
     regenerate: bool = False
-) -> tuple[str, list]:
+) -> list:
     """聊天"""
+    history = [] if history is None else history
     # 重新生成时要把最后的query和response弹出,重用query
     if regenerate:
         # 有历史就重新生成,没有历史就返回空
         if len(history) > 0:
             query, _ = history.pop(-1)
         else:
-            return "", history
+            return history
     else:
         query = query.replace(' ', '')
         if query == None or len(query) < 1:
-            return "", history
+            return history
 
     print(
         {
@@ -37,23 +38,23 @@ def chat(
     )
 
     history.append([query, str(np.random.randint(1, 100, 10))])
-    return "", history
+    return history
 
 
 def regenerate(
-    history: list,
+    history: None | list,
     max_new_tokens: int = 1024,
     top_p: float = 0.8,
     top_k: int = 40,
     temperature: float = 0.8,
-) -> tuple[str, list]:
+) -> list:
     """重新生成最后一次对话的内容"""
-    # 只返回history
-    return chat("", history, max_new_tokens, top_p, top_k, temperature, regenerate=True)[1]
+    return chat("", history, max_new_tokens, top_p, top_k, temperature, regenerate=True)
 
 
-def revocery(history: list):
+def revocery(history: None | list) -> list:
     """恢复到上一轮对话"""
+    history = [] if history is None else history
     if len(history) > 0:
         history.pop(-1)
     return history
@@ -122,14 +123,28 @@ with block as demo:
         query.submit(
             chat,
             inputs=[query, chatbot, max_new_tokens, top_p, top_k, temperature],
-            outputs=[query, chatbot]
+            outputs=[chatbot]
+        )
+
+        # 清空query
+        query.submit(
+            lambda: gr.Textbox(value=""),
+            [],
+            [query],
         )
 
         # 按钮提交
         submit.click(
             chat,
             inputs=[query, chatbot, max_new_tokens, top_p, top_k, temperature],
-            outputs=[query, chatbot]
+            outputs=[chatbot]
+        )
+
+        # 清空query
+        submit.click(
+            lambda: gr.Textbox(value=""),
+            [],
+            [query],
         )
 
         # 重新生成
@@ -147,7 +162,7 @@ with block as demo:
         )
 
     gr.Markdown("""提醒：<br>
-    1. 使用中如果出现异常，将会在文本输入框进行展示，请不要惊慌。 <br>
+    1. 使用中如果出现异常，将会在文本输入框进行展示，请不要惊慌。<br>
     """)
 
 # threads to consume the request

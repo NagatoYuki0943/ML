e5fc97e30bd27cf964ac1d1a0d9c4832dd689332
NagatoYuki0943 <72508155+NagatoYuki0943@users.noreply.github.com>
2024-06-05 14:28:30 +0800

    Update inputs&outputs.py

--- code/gradio/inputs&outputs.py
+++ code/gradio/inputs&outputs.py
@@ -1,7 +1,7 @@
 import gradio as gr
 
 
-input_list = [
+input_list: list = [
     gr.Textbox(label="Textbox", lines=3, max_lines=7, placeholder="Enter text..."),
     gr.TextArea(label="TextArea", lines=3, max_lines=7, placeholder="Enter text..."),
     gr.Number(label="Number", value=0, scale=15, interactive=True),
@@ -11,14 +11,15 @@ input_list = [
     gr.Dropdown(choices=[1, 2, 3, 4], value=1, type="value", label="Dropdown"),
     gr.Radio(choices=[1, 2, 3, 4], value=1, type="value", label="Radio"),
     gr.ColorPicker(label="Color Picker"),
-    gr.Image(sources=["upload", "webcam"], type="pil", label="Image"),
+    gr.Image(sources=["upload", "webcam", "clipboard"], type="pil", label="Image"),
     gr.Audio(sources=["microphone", "upload"], type="numpy", label="Audio File"),
     gr.Video(sources=["upload", "webcam"], label="Video"),
     gr.File(label="File", type="binary"),
+    gr.UploadButton(label="upload a file", file_types=["image", "audio", "video", "text"]),
     gr.DataFrame(headers=["id", "name", "age"], label="Dataframe"),
 ]
 
-output_list = [
+output_list: list = [
     gr.Textbox(label="Textbox output"),
     gr.TextArea(label="TextArea output"),
     gr.Number(label="Number output"),
@@ -32,11 +33,12 @@ output_list = [
     gr.Audio(label="Audio File output"),
     gr.Video(label="Video output"),
     gr.File(label="File output"),
+    gr.File(label="UploadButton output"),
     gr.DataFrame(label="Dataframe output"),
 ]
 
 
-def input_and_output(*input_data):
+def input_and_output(*input_data) -> tuple:
     return input_data
 
 

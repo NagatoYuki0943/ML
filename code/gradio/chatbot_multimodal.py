1710adf7c1366984f103d1f4cf761642e00edfee
NagatoYuki0943 <lihongtu12138@outlook.com>
2024-07-14 16:55:20 +0800

    实现多模态对话

--- code/gradio/chatbot_multimodal.py
+++ code/gradio/chatbot_multimodal.py
@@ -10,20 +10,42 @@ import time
 def print_like_dislike(x: gr.LikeData):
     print(x.index, x.value, x.liked)
 
+
 def add_message(history, message):
-    for x in message["files"]:
-        history.append(((x,), None))
+    print(message)
+    # {
+    #     'text': 'hello',
+    #     'files': [
+    #         'C:\\Users\\Frostbite\\AppData\\Local\\Temp\\gradio\\91f58cae35f058d5795dad2a32407d02a0805e10\\MMMMMKUN3 2B-1.jpeg',
+    #         'C:\\Users\\Frostbite\\AppData\\Local\\Temp\\gradio\\7b2ef40406377750a86f7b368c760787347f2d9d\\MMMMMKUN3 2B-2.jpeg'
+    #     ]
+    # }
+
+    for file in message["files"]:
+        print(f"file: {file}")
+        history.append([(file,), None])
     if message["text"] is not None:
-        history.append((message["text"], None))
+        history.append([message["text"], None])
+
+    print(f"history: {history}")
+    # history: [
+    #     [('C:\\Users\\Frostbite\\AppData\\Local\\Temp\\gradio\\91f58cae35f058d5795dad2a32407d02a0805e10\\MMMMMKUN3 2B-1.jpeg',), None],
+    #     [('C:\\Users\\Frostbite\\AppData\\Local\\Temp\\gradio\\7b2ef40406377750a86f7b368c760787347f2d9d\\MMMMMKUN3 2B-2.jpeg',), None],
+    #     ['hello', None]
+    # ]
+
     return history, gr.MultimodalTextbox(value=None, interactive=False)
+
 
 def bot(history):
     response = "**That's cool!**"
+    # 添加回答
     history[-1][1] = ""
     for character in response:
         history[-1][1] += character
         time.sleep(0.1)
         yield history
+
 
 with gr.Blocks() as demo:
     chatbot = gr.Chatbot(
@@ -32,7 +54,13 @@ with gr.Blocks() as demo:
         bubble_full_width=False
     )
 
-    chat_input = gr.MultimodalTextbox(interactive=True, file_types=["image"], placeholder="Enter message or upload file...", show_label=False)
+    chat_input = gr.MultimodalTextbox(
+        file_types=["image"],
+        file_count='multiple', # 指的是一次上传几张,选择single也可以多次选择
+        placeholder="Enter message or upload file...",
+        label="Prompt",
+        interactive=True,
+    )
 
     chat_msg = chat_input.submit(add_message, [chatbot, chat_input], [chatbot, chat_input])
     bot_msg = chat_msg.then(bot, chatbot, chatbot, api_name="bot_response")

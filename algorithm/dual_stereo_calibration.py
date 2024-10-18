3f9072a1294409f082f899685d62394edf8b343a
algo2 <algo2@example.com>
2024-10-18 16:10:00 +0800

    更新畸变矫正代码

--- algorithm/dual_stereo_calibration.py
+++ algorithm/dual_stereo_calibration.py
@@ -1,42 +1,76 @@
-from loguru import logger
 import cv2
 import numpy as np
 import pandas as pd
 from PIL import Image
 import matplotlib.pyplot as plt
+from loguru import logger
 
 
 class DualStereoCalibration:
     def __init__(
         self,
-        camera_matrix_left: np.ndarray | list[list[float]],
-        camera_matrix_right: np.ndarray | list[list[float]],
-        distortion_coefficients_left: np.ndarray | list[list[float]],
-        distortion_coefficients_right: np.ndarray | list[list[float]],
-        R: np.ndarray | list[list[float]],
-        T: np.ndarray | list[list[float]],
-        pixel_width_mm: float,
+        camera_matrix_left: np.ndarray | list[list[float]] | None = None,
+        camera_matrix_right: np.ndarray | list[list[float]] | None = None,
+        distortion_coefficients_left: np.ndarray | list[list[float]] | None = None,
+        distortion_coefficients_right: np.ndarray | list[list[float]] | None = None,
+        R: np.ndarray | list[list[float]] | None = None,
+        T: np.ndarray | list[list[float]] | None = None,
+        pixel_width_mm: float | None = None,
     ):
-        self.camera_matrix_left = np.array(camera_matrix_left)
-        self.camera_matrix_right = np.array(camera_matrix_right)
-        self.distortion_coefficients_left = np.array(distortion_coefficients_left)
-        self.distortion_coefficients_right = np.array(distortion_coefficients_right)
-        self.R = np.array(R)
-        self.T = np.array(T)
-        self.pixel_width_mm = pixel_width_mm
+        self.camera_matrix_left = np.array(camera_matrix_left) if camera_matrix_left is not None else None
+        self.camera_matrix_right = np.array(camera_matrix_right) if camera_matrix_right is not None else None
+        self.distortion_coefficients_left = np.array(distortion_coefficients_left) if distortion_coefficients_left is not None else None
+        self.distortion_coefficients_right = np.array(distortion_coefficients_right) if distortion_coefficients_right is not None else None
+        self.R = np.array(R) if R is not None else None
+        self.T = np.array(T) if T is not None else None
+        self.pixel_width_mm = pixel_width_mm if pixel_width_mm is not None else None
 
-    def rectify_images(self, image_left: np.ndarray, image_right: np.ndarray):
+    def undistort_images(
+        self, image_left: np.ndarray, image_right: np.ndarray
+    ) -> tuple[
+        np.ndarray,
+        np.ndarray,
+        np.ndarray,
+        np.ndarray,
+        np.ndarray | None,
+        np.ndarray | None,
+        np.ndarray | None,
+        np.ndarray | None,
+        np.ndarray | None,
+        tuple | None,
+        tuple | None,
+    ]:
+        """图像畸变矫正"""
+        if any(
+            [
+                self.camera_matrix_left is None,
+                self.camera_matrix_right is None,
+                self.distortion_coefficients_left is None,
+                self.distortion_coefficients_right is None,
+                self.R is None,
+                self.T is None,
+            ]
+        ):
+            logger.warning("camera matrices not set, couldn't undistort images.")
+            return (
+                image_left,
+                image_right,
+                image_left,
+                image_right,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+            )
+
+        # w, h
         image_size = (image_left.shape[1], image_left.shape[0])
 
-        # 使用 cv2.undistort 进行畸变矫正
-        undistorted_left = cv2.undistort(
-            image_left, self.camera_matrix_left, self.distortion_coefficients_left
-        )
-        undistorted_right = cv2.undistort(
-            image_right, self.camera_matrix_right, self.distortion_coefficients_right
-        )
-        focal_length_pixels_R = self.camera_matrix_right[0, 0]
-        focal_length_pixels_L = self.camera_matrix_left[0, 0]
+        # focal_length_pixels_R = self.camera_matrix_right[0, 0]
+        # focal_length_pixels_L = self.camera_matrix_left[0, 0]
         # logger.info(f"Focal length (in pixels): {focal_length_pixels}")
         # 给定的传感器尺寸和图像分辨率
         # sensor_width_mm = 6.413  # 传感器宽度，以毫米为单位
@@ -46,10 +80,11 @@ class DualStereoCalibration:
         # pixel_width_mm = sensor_width_mm / image_width_pixels
 
         # 将焦距从像素转换为毫米
-        focal_length_mm_R = focal_length_pixels_R * self.pixel_width_mm
-        logger.info("R origin focal", focal_length_mm_R)
-        focal_length_mm_L = focal_length_pixels_L * self.pixel_width_mm
-        logger.info("R origin focal", focal_length_mm_L)
+        # focal_length_mm_R = focal_length_pixels_R * self.pixel_width_mm
+        # logger.info("R origin focal", focal_length_mm_R)
+        # focal_length_mm_L = focal_length_pixels_L * self.pixel_width_mm
+        # logger.info("R origin focal", focal_length_mm_L)
+
         # 获取立体校正参数
         R1, R2, P1, P2, Q, roi1, roi2 = cv2.stereoRectify(
             self.camera_matrix_left,
@@ -59,6 +94,14 @@ class DualStereoCalibration:
             image_size,
             self.R,
             self.T,
+        )
+
+        # 使用 cv2.undistort 进行畸变矫正
+        undistorted_left: np.ndarray = cv2.undistort(
+            image_left, self.camera_matrix_left, self.distortion_coefficients_left
+        )
+        undistorted_right: np.ndarray = cv2.undistort(
+            image_right, self.camera_matrix_right, self.distortion_coefficients_right
         )
 
         # 计算校正映射
@@ -80,6 +123,8 @@ class DualStereoCalibration:
         return (
             rectified_image_left,
             rectified_image_right,
+            undistorted_left,
+            undistorted_right,
             R1,
             R2,
             P1,
@@ -87,20 +132,73 @@ class DualStereoCalibration:
             Q,
             roi1,
             roi2,
-            undistorted_left,
-            undistorted_right,
         )
 
-    def undistort_points(self, points: np.ndarray, camera_matrix, R, P):
-        points = points.reshape(-1, 2)
-        rectified_points = []
+    def undistort_point(
+        self,
+        point: np.ndarray | list[float],
+        camera_matrix: np.ndarray | None,
+        R: np.ndarray | None,
+        P: np.ndarray | None,
+    ) -> list[float]:
+        """对单个点坐标进行畸变矫正
+
+        Args:
+            point (np.ndarray | list[float]): 需要矫正的坐标, 格式为 [x, y]
+            camera_matrix (np.ndarray):
+            R (np.ndarray):
+            P (np.ndarray):
+
+        Returns:
+            list[float]: 矫正后的坐标, 格式为 [x, y]
+        """
+
+        # 将点转换为符合输入格式的数组
+        point = np.array(point)
+
+        if any([camera_matrix is None, R is None, P is None]):
+            logger.warning("camera matrices not set, couldn't undistort points.")
+            return point.tolist()
+
+        undistorted_point: np.ndarray = cv2.undistortPoints(
+            point, camera_matrix, None, R=R, P=P
+        )
+        # 返回矫正后的点坐标
+        point_corrected: list = undistorted_point.ravel().tolist()
+        return point_corrected
+
+    def undistort_points(
+        self,
+        points: np.ndarray | list[list[float]],
+        camera_matrix: np.ndarray | None,
+        R: np.ndarray | None,
+        P: np.ndarray | None,
+    ) -> list[list[float]]:
+        """对多个点坐标进行畸变矫正
+
+        Args:
+            points (np.ndarray | list[list[float]]): 多个坐标, 格式为 [[x1, y1], [x2, y2], ...]
+            camera_matrix (np.ndarray):
+            R (np.ndarray):
+            P (np.ndarray):
+
+        Returns:
+            list[list[float]]: 矫正后的坐标, 格式为 [[x1, y1], [x2, y2], ...]
+        """
+        points = np.array(points).reshape(-1, 2)
+
+        if any([camera_matrix is None, R is None, P is None]):
+            logger.warning("camera matrices not set, couldn't undistort points.")
+            return points.tolist()
+
+        undistorted_points = []
         for point in points:
-            new_point = cv2.undistortPoints(
-                np.array([[point]]), camera_matrix, None, R=R, P=P
+            undistorted_point: np.ndarray = cv2.undistortPoints(
+                point, camera_matrix, None, R=R, P=P
             )
-            x, y = new_point.ravel()
-            rectified_points.append([x, y])
-        return rectified_points
+            point_corrected: list = undistorted_point.ravel().tolist()
+            undistorted_points.append(point_corrected)
+        return undistorted_points
 
     def find_corners(self, rectified_image: np.ndarray, chessboard_size=(11, 8)):
         gray_image = cv2.cvtColor(rectified_image, cv2.COLOR_BGR2GRAY)
@@ -219,4 +317,133 @@ class DualStereoCalibration:
 
 
 if __name__ == "__main__":
-    pass
+    camera_matrix_left = np.array(
+        [
+            [7.44937603e03, 0.00000000e00, 1.79056889e03],
+            [0.00000000e00, 7.45022891e03, 1.26665786e03],
+            [0.00000000e00, 0.00000000e00, 1.00000000e00],
+        ]
+    )
+
+    camera_matrix_right = np.array(
+        [
+            [7.46471035e03, 0.00000000e00, 1.81985040e03],
+            [0.00000000e00, 7.46415680e03, 1.38081032e03],
+            [0.00000000e00, 0.00000000e00, 1.00000000e00],
+        ]
+    )
+
+    distortion_coefficients_left = np.array(
+        [
+            [
+                -4.44924086e-01,
+                6.27814725e-01,
+                -1.80510014e-03,
+                -8.97545764e-04,
+                -1.84473439e01,
+            ]
+        ]
+    )
+    distortion_coefficients_right = np.array(
+        [
+            [
+                -4.07660445e-01,
+                -2.23391154e00,
+                -1.09115383e-03,
+                -3.04516347e-03,
+                7.45504877e01,
+            ]
+        ]
+    )
+
+    R = np.array(
+        [
+            [0.97743098, 0.00689964, 0.21114231],
+            [-0.00564446, 0.99996264, -0.00654684],
+            [-0.2111796, 0.0052073, 0.97743341],
+        ]
+    )
+
+    T = np.array([[-476.53571438], [4.78988367], [49.50495583]])
+    sensor_width_mm = 6.413  # 传感器宽度，以毫米为单位
+    image_width_pixels = 3840  # 图像宽度，以像素为单位
+    # 计算每个像素的宽度（以毫米为单位）
+    pixel_width_mm = sensor_width_mm / image_width_pixels
+
+    # 创建ImageUndistorter对象
+    dual_stereo_calibration = DualStereoCalibration(
+        camera_matrix_left,
+        camera_matrix_right,
+        distortion_coefficients_left,
+        distortion_coefficients_right,
+        R,
+        T,
+        pixel_width_mm,
+    )
+
+    # # 输入图像路径
+    left_image_path = "../assets/template/2circles/2circles-6_5-3-500pixel.png"
+    left_image = cv2.imread(left_image_path)
+    right_image_path = "../assets/template/2circles/2circles-6_5-3-500pixel.png"
+    right_image = cv2.imread(right_image_path)
+    print(left_image.shape)
+    print(right_image.shape)
+
+    # # 输出图像路径
+    left_output_path = "../results/2circles-6_5-3-500pixel_left.jpg"
+    right_output_path1 = "../results/2circles-6_5-3-500pixel_right.jpg"
+
+    # # 调用函数进行图像畸变矫正
+    (
+        rectified_image_left,
+        rectified_image_right,
+        undistorted_left,
+        undistorted_right,
+        R1,
+        R2,
+        P1,
+        P2,
+        Q,
+        roi1,
+        roi2,
+    ) = dual_stereo_calibration.undistort_images(left_image, right_image)
+
+    cv2.imwrite(left_output_path, rectified_image_left)
+    cv2.imwrite(right_output_path1, rectified_image_right)
+
+    # 这里进行模板匹配中心点识别
+    # 输入需要矫正的点坐标
+    xy = [200.0, 200.0]
+    # left 畸变矫正后的坐标点
+    xy_corrected = dual_stereo_calibration.undistort_point(xy, camera_matrix_left, R1, P1)
+    print(f"left 矫正后的点坐标为: {xy_corrected}")
+
+    # right 畸变矫正后的坐标点
+    xy_corrected = dual_stereo_calibration.undistort_point(xy, camera_matrix_right, R2, P2)
+    print(f"right 矫正后的点坐标为: {xy_corrected}")
+
+    xy = [[200.0, 200.0]]
+    # 畸变矫正后的坐标点
+    xy_corrected = dual_stereo_calibration.undistort_point(xy, camera_matrix_left, R1, P1)
+    print(f"left 矫正后的点坐标为: {xy_corrected}")
+
+    xy = [[[200.0, 200.0]]]
+    # 畸变矫正后的坐标点
+    xy_corrected = dual_stereo_calibration.undistort_point(xy, camera_matrix_left, R1, P1)
+    print(f"left 矫正后的点坐标为: {xy_corrected}")
+
+    xy = [[200.0, 200.0], [400.0, 400.0]]
+    # 畸变矫正后的坐标点
+    xy_corrected = dual_stereo_calibration.undistort_points(xy, camera_matrix_left, R1, P1)
+    print(f"left 矫正后的点坐标为: {xy_corrected}")
+
+    print("=" * 100)
+    # 测试为None的情况
+    dual_stereo_calibration = DualStereoCalibration()
+    dual_stereo_calibration.undistort_images(left_image, right_image)
+    xy = [200.0, 200.0]
+    xy_corrected = dual_stereo_calibration.undistort_point(xy, None, None, None)
+    print(f"矫正后的点坐标为: {xy_corrected}")
+    xy = [[200.0, 200.0], [400.0, 400.0]]
+    xy_corrected = dual_stereo_calibration.undistort_points(xy, None, None, None)
+    print(f"矫正后的点坐标为: {xy_corrected}")
--- algorithm/stereo_calibration.py
+++ algorithm/stereo_calibration.py
@@ -1,12 +1,13 @@
 import cv2
 import numpy as np
+from loguru import logger
 
 
 class StereoCalibration:
     def __init__(
         self,
-        K: np.ndarray | list[list[float]],
-        dist_coeffs: np.ndarray | list[list[float]],
+        K: np.ndarray | list[list[float]] | None = None,
+        dist_coeffs: np.ndarray | list[list[float]] | None = None,
     ):
         """初始化图像矫正器
 
@@ -14,13 +15,66 @@ class StereoCalibration:
             K (np.ndarray | list[list[float]]): 相机内参矩阵 (3x3)
             dist_coeffs (np.ndarray | list[list[float]]): 畸变系数 (k1, k2, p1, p2, k3)
         """
-        self.K = np.array(K)
-        self.dist_coeffs = np.array(dist_coeffs)
+        self.K = np.array(K) if K is not None else None
+        self.dist_coeffs = np.array(dist_coeffs) if dist_coeffs is not None else None
 
     def undistort_image(self, image: np.ndarray) -> np.ndarray:
-        """执行图像畸变矫正"""
-        undistorted_img = cv2.undistort(image, self.K, self.dist_coeffs)
+        """图像畸变矫正"""
+        if any([self.K is None, self.dist_coeffs is None]):
+            logger.warning("K or dist_coeffs is None, couldn't undistort image")
+            return image
+
+        undistorted_img: np.ndarray = cv2.undistort(image, self.K, self.dist_coeffs)
         return undistorted_img
+
+    def undistort_point(self, point: np.ndarray | list[float]) -> list[float]:
+        """对单个点坐标进行畸变矫正
+
+        Args:
+            point (np.ndarray | list[float]): 需要矫正的坐标, 格式为 [x, y]
+
+        Returns:
+            list[float]: 矫正后的坐标, 格式为 [x, y]
+        """
+        # 将点转换为符合输入格式的数组
+        point = np.array(point)
+
+        if any([self.K is None, self.dist_coeffs is None]):
+            logger.warning("K or dist_coeffs is None, couldn't undistort point")
+            return point.tolist()
+
+        # 执行点的畸变矫正
+        undistorted_point: np.ndarray = cv2.undistortPoints(
+            point, self.K, self.dist_coeffs, P=self.K
+        )
+
+        # 返回矫正后的点坐标
+        point_corrected: list = undistorted_point.ravel().tolist()
+        return point_corrected
+
+    def undistort_points(self, points: np.ndarray | list[list[float]]) -> list[list[float]]:
+        """对多个点坐标进行畸变矫正
+
+        Args:
+            points (np.ndarray | list[list[float]]): 多个坐标, 格式为 [[x1, y1], [x2, y2], ...]
+
+        Returns:
+            list[list[float]]: 矫正后的坐标, 格式为 [[x1, y1], [x2, y2], ...]
+        """
+        points = np.array(points).reshape(-1, 2)
+
+        if any([self.K is None, self.dist_coeffs is None]):
+            logger.warning("K or dist_coeffs is None, couldn't undistort point")
+            return points.tolist()
+
+        undistorted_points = []
+        for point in points:
+            undistorted_point: np.ndarray = cv2.undistortPoints(
+                point, self.K, self.dist_coeffs, P=self.K
+            )
+            point_corrected: list = undistorted_point.ravel().tolist()
+            undistorted_points.append(point_corrected)
+        return undistorted_points
 
     # def undistort_point(self, u, v):
     #     """
@@ -40,28 +94,6 @@ class StereoCalibration:
     #     # 返回矫正后的点坐标
     #     u_corrected, v_corrected = undistorted_point[0][0]
     #     return u_corrected, v_corrected
-
-    def undistort_point(self, xy: np.ndarray | list[float]) -> np.ndarray:
-        """对单个点坐标进行畸变矫正
-
-        Args:
-            xy (np.ndarray | list[float]): 需要矫正的坐标
-
-        Returns:
-            ndarray: 矫正后的坐标
-        """
-        # 将点转换为符合输入格式的数组
-        # distorted_point = np.array([[xy]])
-        distorted_point = np.array(xy)
-
-        # 执行点的畸变矫正
-        undistorted_point = cv2.undistortPoints(
-            distorted_point, self.K, self.dist_coeffs, P=self.K
-        )
-
-        # 返回矫正后的点坐标
-        xy_corrected: np.ndarray = undistorted_point[0][0]
-        return xy_corrected.tolist()
 
 
 if __name__ == "__main__":
@@ -87,24 +119,50 @@ if __name__ == "__main__":
     )  # 畸变系数
 
     # 创建ImageUndistorter对象
-    undistorter = StereoCalibration(K, dist_coeffs)
+    stereo_calibration = StereoCalibration(K, dist_coeffs)
 
     # 输入图像路径
-    image_path = "../results/image0_default.jpg"
+    image_path = "../assets/template/2circles/2circles-6_5-3-500pixel.png"
     image = cv2.imread(image_path)
+    print(image.shape)
 
     # 输出图像路径
-    output_path = "../results/image0_undistorted.jpg"
+    output_path = "../results/2circles-6_5-3-500pixel.jpg"
 
     # 调用函数进行图像畸变矫正
-    undistorted_image = undistorter.undistort_image(image)
+    undistorted_image = stereo_calibration.undistort_image(image)
 
     cv2.imwrite(output_path, undistorted_image)
 
     # 这里进行模板匹配中心点识别
     # 输入需要矫正的点坐标
-    xy = [1000.0, 1000.0]
-
+    xy = [200.0, 200.0]
     # 畸变矫正后的坐标点
-    xy_corrected = undistorter.undistort_point(xy)
+    xy_corrected = stereo_calibration.undistort_point(xy)
+    print(f"矫正后的点坐标为: {xy_corrected}")
+
+    xy = [[200.0, 200.0]]
+    # 畸变矫正后的坐标点
+    xy_corrected = stereo_calibration.undistort_point(xy)
+    print(f"矫正后的点坐标为: {xy_corrected}")
+
+    xy = [[[200.0, 200.0]]]
+    # 畸变矫正后的坐标点
+    xy_corrected = stereo_calibration.undistort_point(xy)
+    print(f"矫正后的点坐标为: {xy_corrected}")
+
+    xy = [[200.0, 200.0], [400.0, 400.0]]
+    # 畸变矫正后的坐标点
+    xy_corrected = stereo_calibration.undistort_points(xy)
+    print(f"矫正后的点坐标为: {xy_corrected}")
+
+    print("=" * 100)
+    # 测试为None的情况
+    stereo_calibration = StereoCalibration()
+    undistorted_image = stereo_calibration.undistort_image(image)
+    xy = [200.0, 200.0]
+    xy_corrected = stereo_calibration.undistort_point(xy)
+    print(f"矫正后的点坐标为: {xy_corrected}")
+    xy = [[200.0, 200.0], [400.0, 400.0]]
+    xy_corrected = stereo_calibration.undistort_points(xy)
     print(f"矫正后的点坐标为: {xy_corrected}")

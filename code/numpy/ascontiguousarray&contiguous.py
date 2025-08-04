9460b27a01a3b236c7a830e942cd5fb7b6f61cad
NagatoYuki0943 <72508155+NagatoYuki0943@users.noreply.github.com>
2025-08-04 14:30:56 +0800

    Update and rename ascontiguousarray.py to ascontiguousarray&contiguous.py

--- code/numpy/ascontiguousarray&contiguous.py
+++ code/numpy/ascontiguousarray&contiguous.py
@@ -1,12 +1,14 @@
 """
-ascontiguousarray函数将一个内存不连续存储的数组转换为内存连续存储的数组，使得运行速度更快
+array = np.ascontiguousarray(array) 函数将一个内存不连续存储的数组转换为内存连续存储的数组，使得运行速度更快
+tensor = tensor.contiguous() 函数将一个内存不连续存储的tensor转换为内存连续存储的tensor，使得运行速度更快
 """
 
 import numpy as np
+import torch
 
 
-a = np.arange(0, 12, 1)
-print(a.flags)
+arr1 = np.arange(0, 12, 1)
+print(arr1.flags)
 #   C_CONTIGUOUS : True  行连续
 #   F_CONTIGUOUS : True  列连续
 #   OWNDATA : True
@@ -15,8 +17,8 @@ print(a.flags)
 #   WRITEBACKIFCOPY : False
 
 
-b = a.reshape(3, 4)
-print(b.flags)
+arr2 = arr1.reshape(3, 4)
+print(arr2.flags)
 #   C_CONTIGUOUS : True  行连续
 #   F_CONTIGUOUS : False 列不连续
 #   OWNDATA : False
@@ -26,8 +28,8 @@ print(b.flags)
 
 
 # 转置
-c = b.T
-print(c.flags)
+arr3 = arr2.T
+print(arr3.flags)
 #   C_CONTIGUOUS : False 行不连续
 #   F_CONTIGUOUS : True  列连续
 #   OWNDATA : False
@@ -36,8 +38,8 @@ print(c.flags)
 #   WRITEBACKIFCOPY : False
 
 
-d = c[:2, :]
-print(d.flags)
+arr4 = arr3[:2, :]
+print(arr4.flags)
 #   C_CONTIGUOUS : False 行不连续
 #   F_CONTIGUOUS : False 列不连续
 #   OWNDATA : False
@@ -45,11 +47,28 @@ print(d.flags)
 #   ALIGNED : True
 #   WRITEBACKIFCOPY : False
 
-e = np.ascontiguousarray(d)
-print(e.flags)
+arr5 = np.ascontiguousarray(arr4)
+print(arr5.flags)
 #   C_CONTIGUOUS : True  行连续
 #   F_CONTIGUOUS : False 列不连续
 #   OWNDATA : True
 #   WRITEABLE : True
 #   ALIGNED : True
 #   WRITEBACKIFCOPY : False
+
+
+tensor1 = torch.arange(12)
+print(tensor1.is_contiguous())
+# True
+
+tensor2 = tensor1.reshape(3, 4)
+print(tensor2.is_contiguous())
+# True
+
+tensor3 = tensor2.T
+print(tensor3.is_contiguous())
+# False
+
+tensor4 = tensor3.contiguous()
+print(tensor4.is_contiguous())
+# True
